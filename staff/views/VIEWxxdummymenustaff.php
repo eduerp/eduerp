012668bf354d9a7c18a620b6c9df2ffe8c6c787d
Alan Barrett <alanbarrett@ireland.com>
2011-10-22 20:27:56 +0100

    Create views to generate a dummy staff and grading path to get staff/xxx menus to work
    For some reason when installed, student/xxx, staff/xxx and grading/xxx menus
    do not apear in the menus unless there is a student, staff and grading path
    in existance. I do not understand this issue. But this does seem to fix it.
    Note the menus do exist at a depth of 2 with a strange parent, but do not display.
    Single level menus do not seem to work either!

---
+++ staff/views/VIEWxxdummymenustaff.php
@@ -0,0 +1,83 @@
+<?php
+$view = new view;
+$view->name = 'xxdummymenustaff';
+$view->description = '';
+$view->tag = '';
+$view->view_php = '';
+$view->base_table = 'users';
+$view->is_cacheable = FALSE;
+$view->api_version = 2;
+$view->disabled = FALSE; /* Edit this to true to make a default view disabled initially */
+$handler = $view->new_display('default', 'Defaults', 'default');
+$handler->override_option('fields', array(
+  'name' => array(
+    'label' => 'Name',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'link_to_user' => 0,
+    'overwrite_anonymous' => 0,
+    'anonymous_text' => '',
+    'exclude' => 0,
+    'id' => 'name',
+    'table' => 'users',
+    'field' => 'name',
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('filters', array(
+  'uid_current' => array(
+    'operator' => '=',
+    'value' => '1',
+    'group' => '0',
+    'exposed' => FALSE,
+    'expose' => array(
+      'operator' => FALSE,
+      'label' => '',
+    ),
+    'id' => 'uid_current',
+    'table' => 'users',
+    'field' => 'uid_current',
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('access', array(
+  'type' => 'none',
+));
+$handler->override_option('cache', array(
+  'type' => 'none',
+));
+$handler = $view->new_display('page', 'Page', 'page_1');
+$handler->override_option('path', 'staff');
+$handler->override_option('menu', array(
+  'type' => 'none',
+  'title' => '',
+  'description' => '',
+  'weight' => 0,
+  'name' => 'navigation',
+));
+$handler->override_option('tab_options', array(
+  'type' => 'none',
+  'title' => '',
+  'description' => '',
+  'weight' => 0,
+  'name' => 'navigation',
+));
