f01a54f1d9a7d03936beb2f4d9e5b2397afa50e2
Kayode Odeyemi <dreyemi@gmail.com>
2011-12-14 08:59:21 +0000

    invoice theming and tests for student clearance

---
+++ eduerp_transaction/modules/uc_edupay/uc_order.tpl.php
@@ -0,0 +1,11 @@
+<?php
+// $Id: uc_order.tpl.php,v 1.1.2.1 2010/07/16 15:45:09 islandusurper Exp $
+
+/**
+ * @file
+ * This file is a dummy template.
+ *
+ * It serves as a reminder to theme authors that they must have a
+ * uc_order.tpl.php file in their theme if they want to override the other
+ * order templates.
+ */
