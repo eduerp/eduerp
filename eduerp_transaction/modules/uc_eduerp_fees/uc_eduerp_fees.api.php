4fb5f412edbbd3d80b33805e5d016916e94ba67d
Alan Barrett <alanbarrett@ireland.com>
2012-03-15 14:43:11 +0000

    Clean up so as to avoid altering schemas and special field processing

--- eduerp_transaction/modules/uc_eduerp_fees/uc_eduerp_fees.api.php
+++ eduerp_transaction/modules/uc_eduerp_fees/uc_eduerp_fees.api.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 /**
  * @file
- * 
+ *
  * Documentation of all hooks in uc_eduerp_fees module
- */ 
+ */
 
  /**
  * Declare fees.
  *
  * This hook is invoked from uc_eduerp_fees_add_to_cart() to get the list fee items to be
- * added to cart. 
+ * added to cart.
  *
  * @param $fees
  *   The fees object which is a node object.
@@ -26,22 +26,4 @@ function hook_eduerp_fees($fees) {
     'options' => 'Any other options array to pass along'
   );
   return $fees;
-}
-/**
- * Alter fees.
- *
- * This hook is invoked from uc_eduerp_fees_pay_fees() to alter the 
- * elements in the fees structure
- *
- * @param $items
- *   The item in the fees to alter. You can also use it to inject
- *   your own fees elements
- *
- * @ingroup uc_eduerp_fees_api_hooks
- * @ingroup hooks
- */
-function hook_eduerp_fees_alter(&$items) {
-  // Items is passed by reference. So you can modify it
-  $value = array();
-  return $value;
 }
